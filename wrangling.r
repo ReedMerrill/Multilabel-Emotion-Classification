0470a71249a8f254d57fb26e9bccb26ee77a9a70
Reed Merrill <reedjmerrill@gmail.com>
2023-12-03 09:20:37 -0700

    ...

--- wrangling.r
+++ wrangling.r
@@ -9,3 +9,10 @@ dt <- dt |> mutate(
 dt_onehot <- dt |> filter(label_count <= 1)
 
 table(dt$label_count)
+
+# solve for the right sample weights
+
+dt_onehot |> group_by(label_count) |> summarise(
+    n = n(),
+    weight = 1 / n
+)
