cae4df2cc3d21fd310b79c3e90abc1e585ade062
Reed Merrill <reedjmerrill@gmail.com>
2023-12-04 13:05:06 -0700

    partially done imlementing sample weights

--- wrangling.r
+++ wrangling.r
@@ -2,21 +2,9 @@ library(tidyverse)
 
 dt <- read_csv("data/train.csv")
 
-dt <- dt |> mutate(
-    label_count = admiration + amusement + gratitude + love + pride + relief + remorse #nolint
-)
-
-dt_onehot <- dt |> filter(label_count <= 1)
-
-table(dt$label_count)
-
 # get the unique number of label combinations
 dt$label_strings <- paste(
     dt$admiration, dt$amusement, dt$gratitude, dt$love, dt$pride, dt$relief, dt$remorse, sep = "") #nolint
-
-print(paste("Number of unique label combinations:", length(unique(dt$label_strings))))
-print("Unique label combinations:")
-print(unique(dt$label_strings))
 
 # solve for the proportional sample weights
 weights <- dt |>
@@ -24,5 +12,12 @@ weights <- dt |>
     summarise(
         n = n(),
         weight = (1 / n) * 10
-)
-weights
+) |> select(-c(n))
+
+# join weights to the original data
+dt <- dt |>
+    left_join(weights, by = "label_strings") |>
+    select(-label_strings) |>
+    relocate(weight, .before = text)
+
+write_csv(dt, "data/train_weights.csv")
