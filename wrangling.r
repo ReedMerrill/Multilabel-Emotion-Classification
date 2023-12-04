ba7b8e99950bfcbfc22fa887d14db6e11dd78444
Reed Merrill <reedjmerrill@gmail.com>
2023-12-04 09:37:05 -0700

    begin test phase

--- wrangling.r
+++ wrangling.r
@@ -10,9 +10,19 @@ dt_onehot <- dt |> filter(label_count <= 1)
 
 table(dt$label_count)
 
-# solve for the right sample weights
+# get the unique number of label combinations
+dt$label_strings <- paste(
+    dt$admiration, dt$amusement, dt$gratitude, dt$love, dt$pride, dt$relief, dt$remorse, sep = "") #nolint
 
-dt_onehot |> group_by(label_count) |> summarise(
-    n = n(),
-    weight = 1 / n
+print(paste("Number of unique label combinations:", length(unique(dt$label_strings))))
+print("Unique label combinations:")
+print(unique(dt$label_strings))
+
+# solve for the proportional sample weights
+weights <- dt |>
+    group_by(label_strings) |>
+    summarise(
+        n = n(),
+        weight = (1 / n) * 10
 )
+weights
